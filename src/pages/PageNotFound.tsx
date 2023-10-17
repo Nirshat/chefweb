8452bc29cb13363bcd5e72ca591f5636569bab92
codeyron <92419200+Nirshat@users.noreply.github.com>
2023-10-17 14:26:48 +0800

    fixing routes

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -5,7 +5,7 @@ const PageNotFound = () => {
     <div className="rounded min-h-screen flex flex-col items-center justify-center">
       <div className="rounded text-3xl">
         <span className="font-bold text-orange-600">404. </span>
-        <span className="text-blue-800"> Page not found.</span>
+        <span className="text-blue-800 font-semibold"> Page not found.</span>
       </div>
     </div>
   )
