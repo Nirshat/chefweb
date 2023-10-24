882f8a9ba9b0f4f3beb265328658bd47745ae1ab
codeyron <92419200+Nirshat@users.noreply.github.com>
2023-10-24 17:23:34 +0800

    revamp :hamburger::pizza::hotdog:

--- src/stores/useResultStore.ts
+++ src/stores/useResultStore.ts
@@ -7,20 +7,16 @@ type State = {
     strMeal: string
     strMealThumb: string
   }[]
-  loading: boolean
 }
 
 type Action = {
   updateResults: (res: State['results']) => void
-  setLoading: (stat: State['loading']) => void
 }
 
 const useResultStore = create<State & Action>((set) => (
   {
     results: [],
     updateResults: (res) => set((state) => ({...state, results: [...res]})),
-    loading: false,
-    setLoading: (stat) => set((state) => ({...state, loading:stat})),
   }
 ));
 
